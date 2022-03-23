3e7df87d511dcbcc9e6159f5b3c5bbd7fc32ba5b
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-03-23 21:28:10 +0800

    feat(Ninja):浅尝NinjaAPI

--- apps/oauth/urls.py
+++ apps/oauth/urls.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from django.urls import path
-
+from ninja import *
 from oauth.views.index_views import *
 from oauth.views.signin_views import *
 from oauth.views.user_views import *
+
+api = NinjaAPI()
+
+
+@api.get("/add")
+def add(request, a: int, b: int):
+    return {"result": a + b}
+
 
 urlpatterns = [
     path('', SignInView.as_view(), name='login'),
@@ -15,5 +23,5 @@ urlpatterns = [
     path('user-add', UserCreateView.as_view(), name='user_add'),  # 新增用户
     path('user-update/<int:pk>/', UserUpdateView.as_view(), name="user_update"),  # 更新用户
     path('user-delete/<int:pk>/', UserDeleteView.as_view(), name="user_delete"),  # 删除用户
-
+    path("api/", api.urls, name="api"),
 ]
